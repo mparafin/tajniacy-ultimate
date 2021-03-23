30854ecee48f72ac006ab98a412512db027cf235
Michał Parafiniuk <michal.a.parafiniuk@gmail.com>
2021-03-23 20:58:21 +0100

    few first steps (up to team change buttons)

--- backend/test.py
+++ backend/test.py
@@ -1,26 +1,86 @@
 import sys
 import asyncio
 import websockets
+import json
+import enum
 
-wss = set()
+class Team(enum.Enum):
+	SPEC = 0
+	RED = 1
+	BLUE = 2
 
-async def hello(websocket, path):
+class Player:
+	def __init__(self, websocket):
+		self.socket = websocket
+		self.nick = ""
+		self.team = Team.RED
+		self.capt = False
+
+PLAYERS = set()
+
+def player_list(player):
+	result = {}
+	result["type"] = "player_list"
+	result["red"] = list()
+	result["blue"] = list()
+	result["spec"] = list()
+	for p in PLAYERS:
+		if p.nick == "" or p.nick == player.nick:
+			continue
+		{
+			Team.RED: lambda p: result["red"].append(p.nick),
+			Team.BLUE: lambda p: result["blue"].append(p.nick),
+			Team.SPEC: lambda p: result["spec"].append(p.nick),
+		}[p.team](p)
+	result["player"] = {"nick":player.nick, "team":player.team.name}
+	return json.dumps(result)
+
+async def broadcast_player_list():
+	await asyncio.gather(*[p.socket.send(player_list(p)) for p in PLAYERS])
+
+async def broadcast(message):
+	await asyncio.gather(*[p.socket.send(message) for p in PLAYERS])
+
+async def name_handler(message, player):
+	print("Changing player name from " + player.nick + " to " + message["nick"])
+	player.nick = message["nick"]
+	await broadcast_player_list()
+
+async def click_handler(message, player):
+	print("click_handler")
+	if message["id"] == "0 0":
+		await broadcast("woof!")
+
+async def teamchange_handler(message, player):
+	print("Changing team of player " + player.nick + " to " + message["team"])
+	player.team = {'red': Team.RED, 'blue': Team.BLUE, 'spec': Team.SPEC}[message["team"]]
+	await broadcast_player_list()
+
+async def message_handler(message, player):
+	await {
+		'nick': name_handler,
+		'click': click_handler,
+		'teamchange': teamchange_handler,
+	}[message["type"]](message, player)
+
+async def client_handler(websocket, path):
 	print("CONNECTION WITH CLIENT ESTABLISHED")
-	wss.add(websocket)
-	await websocket.send("What's your name?")
-	name = await websocket.recv()
-	print(f"< {name}")
-
-	greeting = f"Hello {name}!"
-
-	await websocket.send(greeting)
-	print(f"> {greeting}")
+	p = Player(websocket)
+	PLAYERS.add(p)
+	await websocket.send(player_list(p))
+	
 	print("Entering echo mode")
-	async for message in websocket:
-		await websocket.send(f"OK... {message}")
+	try:
+		async for message in websocket:
+			mes = json.loads(message)
+			await message_handler(mes, p)			
+			await websocket.send(json.dumps({"type":"misc", "data":json.dumps(mes)}))
+	finally:
+		print("CLIENT DISCONNECTED")
+		PLAYERS.remove(p)
 
 async def main():
-	start_server = websockets.serve(hello, "localhost", 8888)
+	start_server = websockets.serve(client_handler, "localhost", 8888)
 	thing = await start_server
 	print("server up")
 
--- front/script.js
+++ front/script.js
@@ -1,22 +1,63 @@
-console.log("huhuhuuu!");
-var myHeading = document.querySelector("h1");
-myHeading.textContent = "Huhuhuhuuu!";
-document.querySelector("h1").onclick = function () {
-    alert("Yeah, baby!");
+socket = new WebSocket('ws://127.0.0.1:8888');
+teamNames = ["red", "blue", "spec"]
+
+document.getElementById("namechanger").onclick = function () {
+	let nick = document.getElementById("nick").value;
+	socket.send(JSON.stringify({"type":"nick", "nick":nick}));
 }
 
-socket = new WebSocket('ws://127.0.0.1:8888');
+teamNames.forEach(team => {
+	document.getElementById("join"+team).onclick = function () {
+		socket.send(JSON.stringify({"type":"teamchange", "team":team}));
+	}
+})
 
-function send_message(node) {
-    socket.send(node.textContent);
+matrix = document.getElementById("matrix");
+matrix.setAttribute('border', '1');
+for (let i = 0; i < 5; i++) {
+	let tr = document.createElement("tr");
+	for (let j = 0; j < 5; j++) {
+		let td = document.createElement("td");
+		td.id = i + " " + j;
+		td.style.textAlign = 'center';
+		td.style.verticalAlign = 'middle';
+		td.textContent = "I am cell " + td.id;
+		td.onclick = function () {
+			socket.send(JSON.stringify({"type":"click", "id":td.id}));
+		}
+		tr.appendChild(td);
+	}
+	matrix.appendChild(tr);
 }
 
 socket.onmessage = function(s) {
-    console.log("Message from server! : " + s.data);
-    let newp = document.createElement("p");
-    newp.textContent = s.data;
-    newp.onclick = function () {
-        send_message(newp);
-    }
-    document.querySelector("body").appendChild(newp);
+	message = JSON.parse(s.data)
+	switch(message["type"]) {
+		case "nick":
+			console.log("Changed player nickname to: " + message["nick"]);
+			break;
+		case "player_list":
+			teamNames.forEach(team => {
+				elementName = team + "team";
+				let t = document.getElementById(elementName);
+				if(t != null) {
+					while(t.firstChild) {
+						t.removeChild(t.lastChild);
+					}
+				}
+				if (message["player"].team.toLowerCase() === team) {
+					p = document.createElement("div");
+					p.textContent = message["player"].nick;
+					p.style.color = "rgb(0, 100, 10)";
+					t.appendChild(p);
+				}
+				message[team].forEach(player => {
+					p = document.createElement("div");
+					p.textContent = player;
+					t.appendChild(p);
+				})
+			});
+	}
+
+	console.log("Message from server! : " + s.data);
 }
